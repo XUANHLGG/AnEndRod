4d8ec4d669df786e2e912bc4d07e01adbd2e7748
WHITECN2009 <2603843916@qq.com>
2025-12-20 16:49:38 +0800

    v2.0bprePublish2

--- src/main/java/org/WHITECN/utils/AdvancementHandler.java
+++ src/main/java/org/WHITECN/utils/AdvancementHandler.java
@@ -1,35 +1,19 @@
 package org.WHITECN.utils;
 
-import net.md_5.bungee.api.ChatColor;
-import net.md_5.bungee.api.chat.TextComponent;
 import org.bukkit.Bukkit;
 import org.bukkit.Sound;
 import org.bukkit.entity.Player;
 
 public class AdvancementHandler {
     public static void onUsed1kTimes(Player player){
-        TextComponent message = new TextComponent();
-        TextComponent playerName = new TextComponent(player.getName());
-        TextComponent advancementText = new TextComponent("[要...要坏掉了喵T^T]");
-        playerName.setInsertion(player.getName());
-        advancementText.setColor(ChatColor.GREEN);
-        message.addExtra(playerName);
-        message.addExtra("取得了进度");
-        message.addExtra(advancementText);
-        Bukkit.broadcastMessage(message.toString());
+        String message = player.getName() + "取得了进度" + "§a[要...要坏掉了喵T^T]";
+        Bukkit.broadcastMessage(message);
         player.playSound(player, Sound.UI_TOAST_IN,100.0f, 1.0f);
     }
 
     public static void onUsed10kTimes(Player player){
-        TextComponent message = new TextComponent();
-        TextComponent playerName = new TextComponent(player.getName());
-        TextComponent advancementText = new TextComponent("[已...已经坏掉了T^T]");
-        playerName.setInsertion(player.getName());
-        advancementText.setColor(ChatColor.DARK_PURPLE);
-        message.addExtra(playerName);
-        message.addExtra("完成了挑战");
-        message.addExtra(advancementText);
-        Bukkit.broadcastMessage(message.toString());
+        String message = player.getName() + "完成了挑战" + "§5[已...已经坏掉了T^T]";
+        Bukkit.broadcastMessage(message);
         player.playSound(player, Sound.UI_TOAST_CHALLENGE_COMPLETE,80.0f, 1.0f);
     }
 
